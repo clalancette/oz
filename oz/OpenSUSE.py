bb100beca7cbd05bb38011435062764fa09a7fe6
Chris Lalancette <clalance@redhat.com>
2010-11-04 15:59:20 -0400

    Add OpenSUSE 11.3 as an install option.
    Signed-off-by: Chris Lalancette <clalance@redhat.com>

---
+++ oz/OpenSUSE.py
@@ -0,0 +1,67 @@
+import Guest
+import re
+import shutil
+import ozutil
+
+class OpenSUSEGuest(Guest.CDGuest):
+    def __init__(self, update, arch, url, autoyast):
+        Guest.CDGuest.__init__(self, "OpenSUSE", update, arch, "rtl8139", None, None, None)
+        self.isourl = url
+        self.autoyast = autoyast
+
+    def modify_iso(self):
+        self.log.debug("Putting the autoyast in place")
+        shutil.copy(self.autoyast, self.iso_contents + "/autoinst.xml")
+
+        self.log.debug("Modifying the boot options")
+        f = open(self.iso_contents + "/boot/" + self.arch + "/loader/isolinux.cfg", "r")
+        lines = f.readlines()
+        f.close()
+        for line in lines:
+            if re.match("timeout", line):
+                lines[lines.index(line)] = "timeout 1\n"
+            elif re.match("default", line):
+                lines[lines.index(line)] = "default customiso\n"
+        lines.append("label customiso\n")
+        lines.append("  kernel linux\n")
+        lines.append("  append initrd=initrd splash=silent instmode=cd autoyast=default")
+
+        f = open(self.iso_contents + "/boot/" + self.arch + "/loader/isolinux.cfg", "w")
+        f.writelines(lines)
+        f.close()
+
+    def generate_new_iso(self):
+        self.log.info("Generating new ISO")
+        Guest.subprocess_check_output(["mkisofs", "-r", "-J", "-V", "Custom",
+                                       "-l", "-b",
+                                       "boot/" + self.arch + "/loader/isolinux.bin",
+                                       "-c", "boot/" + self.arch + "/loader/boot.cat",
+                                       "-no-emul-boot", "-boot-load-size", "4",
+                                       "-boot-info-table", "-graft-points",
+                                       "-iso-level", "4", "-pad",
+                                       "-allow-leading-dots",
+                                       "-o", self.output_iso,
+                                       self.iso_contents])
+
+    def generate_install_media(self):
+        self.log.info("Generating install media")
+        self.get_original_iso(self.isourl)
+        self.copy_iso()
+        self.modify_iso()
+        self.generate_new_iso()
+        self.cleanup_iso()
+
+def get_class(idl):
+    update = idl.update()
+    arch = idl.arch()
+    autoyast = ozutil.generate_full_auto_path("opensuse-" + update + "-jeos.xml")
+
+    if idl.installtype() != 'iso':
+        raise Exception, "OpenSUSE installs must be done via ISO"
+
+    url = ozutil.check_url(idl.iso())
+
+    if update == "11.3":
+        return OpenSUSEGuest(update, arch, url, autoyast)
+
+    raise Exception, "Unsupported OpenSUSE update " + update
