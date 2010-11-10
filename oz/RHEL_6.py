e519c7ab93a5c759a179970e49dc60fed97c5d15
Chris Lalancette <clalance@redhat.com>
2010-11-10 17:46:11 -0500

    RHEL-6 support.
    Signed-off-by: Chris Lalancette <clalance@redhat.com>

---
+++ oz/RHEL_6.py
@@ -0,0 +1,99 @@
+# Copyright (C) 2010  Chris Lalancette <clalance@redhat.com>
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+import Guest
+import shutil
+import subprocess
+import re
+import ozutil
+
+class RHEL6Guest(Guest.CDGuest):
+    def __init__(self, idl, config):
+        update = idl.update()
+        arch = idl.arch()
+        key = idl.key()
+        self.ks_file = ozutil.generate_full_auto_path("rhel-6-jeos.ks")
+        self.installtype = idl.installtype()
+
+        if self.installtype == 'url':
+            self.url = idl.url()
+        elif self.installtype == 'iso':
+            self.url = idl.iso()
+        else:
+            raise Exception, "RHEL-6 installs must be done via url or iso"
+
+        self.url = ozutil.check_url(self.url)
+
+        if self.installtype == 'url':
+            ozutil.deny_localhost(self.url)
+        # FIXME: if doing an ISO install, we have to check that the ISO passed
+        # in is the DVD, not the CD (since we can't change disks midway)
+
+        Guest.CDGuest.__init__(self, "RHEL-6", update, arch, "virtio", None,
+                               None, "virtio", config)
+
+    def modify_iso(self):
+        self.log.debug("Putting the kickstart in place")
+
+        shutil.copy(self.ks_file, self.iso_contents + "/ks.cfg")
+
+        self.log.debug("Modifying the boot options")
+        f = open(self.iso_contents + "/isolinux/isolinux.cfg", "r")
+        lines = f.readlines()
+        f.close()
+        for line in lines:
+            if re.match("timeout", line):
+                lines[lines.index(line)] = "timeout 1\n"
+            elif re.match("default", line):
+                lines[lines.index(line)] = "default customiso\n"
+        lines.append("label customiso\n")
+        lines.append("  kernel vmlinuz\n")
+        initrdline = "  append initrd=initrd.img ks=cdrom:/ks.cfg method="
+        if self.installtype == "url":
+            initrdline += self.url + "\n"
+        else:
+            initrdline += "cdrom:/dev/cdrom\n"
+        lines.append(initrdline)
+
+        f = open(self.iso_contents + "/isolinux/isolinux.cfg", "w")
+        f.writelines(lines)
+        f.close()
+
+    def generate_new_iso(self):
+        self.log.debug("Generating new ISO")
+        Guest.subprocess_check_output(["mkisofs", "-r", "-T", "-J", "-V",
+                                       "Custom", "-b", "isolinux/isolinux.bin",
+                                       "-c", "isolinux/boot.cat",
+                                       "-no-emul-boot", "-boot-load-size", "4",
+                                       "-boot-info-table", "-v", "-v",
+                                       "-o", self.output_iso, self.iso_contents])
+
+    def generate_install_media(self, force_download):
+        self.log.info("Generating install media")
+        fetchurl = self.url
+        if self.installtype == 'url':
+            fetchurl += "/images/boot.iso"
+        self.get_original_iso(fetchurl, force_download)
+        self.copy_iso()
+        self.modify_iso()
+        self.generate_new_iso()
+        self.cleanup_iso()
+
+def get_class(idl, config):
+    update = idl.update()
+    if update == "0":
+        return RHEL6Guest(idl, config)
+    raise Exception, "Unsupported RHEL-6 update " + update
