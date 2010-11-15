bc15499123d57886f839797d714c2f0e850a8bd9
Chris Lalancette <clalance@redhat.com>
2010-11-15 14:29:34 -0500

    Add CDL generation support to FedoraCore.
    Signed-off-by: Chris Lalancette <clalance@redhat.com>

--- oz/FedoraCore.py
+++ oz/FedoraCore.py
@@ -19,6 +19,7 @@ import shutil
 import subprocess
 import re
 import ozutil
+import os
 
 class FedoraCoreGuest(Guest.CDGuest):
     def __init__(self, idl, config):
@@ -90,6 +91,205 @@ class FedoraCoreGuest(Guest.CDGuest):
         self.modify_iso()
         self.generate_new_iso()
         self.cleanup_iso()
+
+    def get_default_runlevel(self):
+        runlevel = "3"
+        if self.g.exists('/etc/inittab'):
+            lines = self.g.cat('/etc/inittab').split("\n")
+            for line in lines:
+                if re.match('id:', line):
+                    # FIXME: this parsing is a bit iffy
+                    runlevel = line.split(':')[1]
+                    break
+
+        return runlevel
+
+    def get_service_runlevel_link(self, runlevel, service):
+        lines = self.g.cat('/etc/init.d/' + service).split("\n")
+        startlevel = "99"
+        for line in lines:
+            if re.match('# chkconfig:', line):
+                # FIXME: this parsing could get ugly fast
+                startlevel = line.split(':')[1].split()[1]
+                break
+
+        return "/etc/rc.d/rc" + runlevel + ".d/S" + startlevel + service
+
+    def collect_setup(self, libvirt_xml):
+        self.log.info("CDL Collection Setup")
+
+        self.guestfs_handle_setup(libvirt_xml)
+
+        # we have to do 3 things to make sure we can ssh into Fedora 13:
+        # 1)  Upload our ssh key
+        # 2)  Make sure sshd is running on boot
+        # 3)  Make sure that port 22 is open in the firewall
+        # 4)  Make the guest announce itself to the host
+
+        runlevel = self.get_default_runlevel()
+
+        # part 1; upload the keys
+        self.log.debug("Step 1: Uploading ssh keys")
+        if not self.g.exists('/root/.ssh'):
+            self.g.mkdir('/root/.ssh')
+
+        if self.g.exists('/root/.ssh/authorized_keys'):
+            self.g.mv('/root/.ssh/authorized_keys',
+                      '/root/.ssh/authorized_keys.cdl')
+
+        if not os.access(self.cdl_tmp, os.F_OK):
+            os.makedirs(self.cdl_tmp)
+
+        privname = self.cdl_tmp + '/id_rsa-cdl-gen'
+        pubname = self.cdl_tmp + '/id_rsa-cdl-gen.pub'
+        if os.access(privname, os.F_OK):
+            os.remove(privname)
+        if os.access(pubname, os.F_OK):
+            os.remove(pubname)
+        subprocess.call(['ssh-keygen', '-q', '-t', 'rsa', '-b', '2048',
+                         '-N', '', '-f', privname])
+
+        self.g.upload(pubname, '/root/.ssh/authorized_keys')
+
+        # part 2; check and setup sshd
+        self.log.debug("Step 2: setup sshd")
+        if not self.g.exists('/etc/init.d/sshd') or not self.g.exists('/usr/sbin/sshd'):
+            raise Exception, "ssh not installed on the image, cannot continue"
+
+        startuplink = self.get_service_runlevel_link(runlevel, 'sshd')
+        if self.g.exists(startuplink):
+            self.g.mv(startuplink, startuplink + ".cdl")
+        self.g.ln_sf('/etc/init.d/sshd', startuplink)
+
+        sshd_config = \
+"""SyslogFacility AUTHPRIV
+PasswordAuthentication yes
+ChallengeResponseAuthentication no
+GSSAPIAuthentication yes
+GSSAPICleanupCredentials yes
+UsePAM yes
+AcceptEnv LANG LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES
+AcceptEnv LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT
+AcceptEnv LC_IDENTIFICATION LC_ALL LANGUAGE
+AcceptEnv XMODIFIERS
+X11Forwarding yes
+Subsystem	sftp	/usr/libexec/openssh/sftp-server
+"""
+
+        sshd_config_file = self.cdl_tmp + "/sshd_config"
+        f = open(sshd_config_file, 'w')
+        f.write(sshd_config)
+        f.close()
+
+        if self.g.exists('/etc/ssh/sshd_config'):
+            self.g.mv('/etc/ssh/sshd_config', '/etc/ssh/sshd_config.cdl')
+        self.g.upload(sshd_config_file, '/etc/ssh/sshd_config')
+        os.unlink(sshd_config_file)
+
+        # part 3; open up iptables
+        self.log.debug("Step 3: Open up the firewall")
+        if self.g.exists('/etc/sysconfig/iptables'):
+            self.g.mv('/etc/sysconfig/iptables', '/etc/sysconfig/iptables.cdl')
+        # implicit else; if there is no iptables file, the firewall is open
+
+        # part 4; make sure the guest announces itself
+        self.log.debug("Step 4: Guest announcement")
+        if not self.g.exists('/etc/init.d/crond') or not self.g.exists('/usr/sbin/crond'):
+            raise Exception, "cron not installed on the image, cannot continue"
+
+        cdlpath = ozutil.generate_full_guesttools_path('cdl-nc')
+        self.g.upload(cdlpath, '/root/cdl-nc')
+        self.g.chmod(0755, '/root/cdl-nc')
+
+        announcefile = self.cdl_tmp + "/announce"
+        f = open(announcefile, 'w')
+        f.write('*/1 * * * * root /bin/bash -c "/root/cdl-nc ' + self.host_bridge_ip + ' ' + str(self.listen_port) + '"\n')
+        f.close()
+
+        self.g.upload(announcefile, '/etc/cron.d/announce')
+
+        startuplink = self.get_service_runlevel_link(runlevel, 'crond')
+        if self.g.exists(startuplink):
+            self.g.mv(startuplink, startuplink + ".cdl")
+        self.g.ln_sf('/etc/init.d/crond', startuplink)
+
+        os.unlink(announcefile)
+        self.guestfs_handle_cleanup()
+
+    def collect_teardown(self, libvirt_xml):
+        self.log.info("CDL Collection Teardown")
+
+        self.guestfs_handle_setup(libvirt_xml)
+
+        # reset the authorized keys
+        if self.g.exists('/root/.ssh/authorized_keys'):
+            self.g.rm('/root/.ssh/authorized_keys')
+        if self.g.exists('/root/.ssh/authorized_keys.cdl'):
+            self.g.mv('/root/.ssh/authorized_keys.cdl',
+                      '/root/.ssh/authorized_keys')
+
+        # reset iptables
+        if self.g.exists('/etc/sysconfig/iptables'):
+            self.g.rm('/etc/sysconfig/iptables')
+        if self.g.exists('/etc/sysconfig/iptables.cdl'):
+            self.g.mv('/etc/sysconfig/iptables')
+
+        # remove announce cronjob
+        if self.g.exists('/etc/cron.d/announce'):
+            self.g.rm('/etc/cron.d/announce')
+
+        # remove cdl-nc binary
+        if self.g.exists('/root/cdl-nc'):
+            self.g.rm('/root/cdl-nc')
+
+        # remove custom sshd_config
+        if self.g.exists('/etc/ssh/sshd_config'):
+            self.g.rm('/etc/ssh/sshd_config')
+        if self.g.exists('/etc/ssh/sshd_config.cdl'):
+            self.g.mv('/etc/ssh/sshd_config.cdl', '/etc/ssh/sshd_config')
+
+        # reset the service links
+        runlevel = self.get_default_runlevel()
+
+        for service in ["sshd", "crond"]:
+            startuplink = self.get_service_runlevel_link(runlevel, service)
+            if self.g.exists(startuplink):
+                self.g.rm(startuplink)
+            if self.g.exists(startuplink + ".cdl"):
+                self.g.mv(startuplink + ".cdl", startuplink)
+
+        self.guestfs_handle_cleanup()
+
+    def generate_cdl(self, libvirt_xml):
+        self.log.info("Generating CDL")
+
+        self.collect_setup(libvirt_xml)
+
+        output = ''
+        try:
+            self.libvirt_dom = self.libvirt_conn.defineXML(libvirt_xml)
+            self.libvirt_dom.create()
+
+            guestaddr = self.wait_for_guest_boot()
+
+            data = subprocess.Popen(["ssh", "-i",
+                                     self.cdl_tmp + '/id_rsa-cdl-gen',
+                                     "-o", "StrictHostKeyChecking=no",
+                                     "-o", "ConnectTimeout=5", guestaddr,
+                                     'rpm -qa'], stdout=subprocess.PIPE).communicate()
+
+            # FIXME: what if the output is blank?
+
+            output = self.output_cdl_xml(data[0].split("\n"))
+
+            # FIXME: should we try to do a graceful shutdown here?  At the very
+            # least we should do a sync
+        finally:
+            if self.libvirt_dom is not None:
+                self.libvirt_dom.destroy()
+            self.collect_teardown(libvirt_xml)
+
+        return output
 
 class FedoraCore4Guest(FedoraCoreGuest):
     def generate_diskimage(self):
