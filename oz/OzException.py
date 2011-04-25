e7697ef66fcb6559691ffe28d8e54917cd4f632a
Chris Lalancette <clalance@redhat.com>
2011-04-25 11:07:47 -0400

    Add pydoc documentation for OzException.
    Signed-off-by: Chris Lalancette <clalance@redhat.com>

--- oz/OzException.py
+++ oz/OzException.py
@@ -14,6 +14,13 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
+"""
+Exception class for Oz.
+"""
+
 class OzException(Exception):
+    """
+    Class for Oz Exceptions.
+    """
     def __init__(self, msg):
         Exception.__init__(self, msg)
