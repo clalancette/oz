40edbfdefa710e0963ae816c7506c0aa4b64a283
Chris Lalancette <clalance@redhat.com>
2011-06-17 17:01:46 -0400

    Add Ubuntu 6.06 support.
    Signed-off-by: Chris Lalancette <clalance@redhat.com>

--- oz/Ubuntu.py
+++ oz/Ubuntu.py
@@ -36,9 +36,9 @@ class UbuntuGuest(oz.Guest.CDGuest):
         self.url = self.check_url(self.tdl, iso=True, url=False)
 
     def check_iso_tree(self):
-        if self.tdl.update in ["6.10", "7.04"]:
+        if self.tdl.update in ["6.06", "6.06.1", "6.06.2", "6.10", "7.04"]:
             if os.path.isdir(os.path.join(self.iso_contents, "casper")):
-                raise oz.OzException.OzException("Ubuntu 6.10 and 7.04 installs can only be done using the alternate or server CDs")
+                raise oz.OzException.OzException("Ubuntu %s installs can only be done using the alternate or server CDs" % (self.tdl.update))
 
     def modify_iso(self):
         self.log.debug("Modifying ISO")
@@ -74,8 +74,11 @@ class UbuntuGuest(oz.Guest.CDGuest):
             f.write("  kernel /casper/vmlinuz\n")
             f.write("  append file=/cdrom/preseed/customiso.seed boot=casper automatic-ubiquity noprompt keyboard-configuration/layoutcode=us initrd=/casper/" + self.casper_initrd + "\n")
         else:
+            keyboard = "console-setup/layoutcode=us"
+            if self.tdl.update in ["6.06", "6.06.1", "6.06.2"]:
+                keyboard = "kbd-chooser/method=us"
             f.write("  kernel /install/vmlinuz\n")
-            f.write("  append file=/cdrom/preseed/customiso.seed debian-installer/locale=en_US console-setup/layoutcode=us netcfg/choose_interface=auto keyboard-configuration/layoutcode=us priority=critical initrd=/install/initrd.gz --\n")
+            f.write("  append preseed/file=/cdrom/preseed/customiso.seed debian-installer/locale=en_US " + keyboard + " netcfg/choose_interface=auto keyboard-configuration/layoutcode=us priority=critical initrd=/install/initrd.gz --\n")
         f.close()
 
     def generate_new_iso(self):
@@ -93,13 +96,13 @@ class UbuntuGuest(oz.Guest.CDGuest):
         return self.iso_generate_install_media(self.url, force_download)
 
     def install(self, timeout=None, force=False):
-        if self.tdl.update in ["6.10", "7.04"]:
+        if self.tdl.update in ["6.06", "6.06.1", "6.06.2", "6.10", "7.04"]:
             if not timeout:
                 timeout = 3000
         return self.do_install(timeout, force, 0)
 
 def get_class(tdl, config, auto):
-    if tdl.update in ["6.10", "7.04", "7.10"]:
+    if tdl.update in ["6.06", "6.06.1", "6.06.2", "6.10", "7.04", "7.10"]:
         return UbuntuGuest(tdl, config, auto, "initrd.gz", "rtl8139", None)
     if tdl.update in ["8.04", "8.04.1", "8.04.2", "8.04.3", "8.04.4", "8.10",
                       "9.04"]:
