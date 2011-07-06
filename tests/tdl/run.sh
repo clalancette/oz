6aa8b7b2417842a85ba6379d3e7930b83fbe13b5
Chris Lalancette <clalance@redhat.com>
2011-07-06 15:36:06 -0400

    Update the test script to re-use some of the code.
    Signed-off-by: Chris Lalancette <clalance@redhat.com>

--- tests/tdl/run.sh
+++ tests/tdl/run.sh
@@ -9,49 +9,50 @@ fi
 SUCCESS=0
 FAIL=0
 
-schema_expect_success() {
-    echo -n "Testing Schema $1..."
-    if [ ! -r "$1" ]; then
-	echo "File not found"
-	return
-    fi
-    if [ $DEBUG -eq 0 ]; then
-	xmllint --noout --relaxng ../../docs/tdl.rng "$1" >& /dev/null
-    else
-	xmllint --relaxng ../../docs/tdl.rng "$1"
-    fi
-    if [ $? -eq 0 ]; then
-	echo "OK"
-	SUCCESS=$(( $SUCCESS + 1 ))
-    else
-	echo "FAIL"
-	FAIL=$(( $FAIL + 1 ))
-    fi
+success() {
+    echo "OK"
+    SUCCESS=$(( $SUCCESS + 1 ))
 }
-schema_expect_fail() {
-    echo -n "Testing Schema $1..."
+
+failure() {
+    echo "FAIL"
+    FAIL=$(( $FAIL + 1 ))
+}
+
+schema_test() {
+    schema="$1"
+    expectsuccess="$2"
+
+    echo -n "Testing Schema $schema..."
+
     if [ ! -r "$1" ]; then
-	echo "File not found"
+	echo "FAIL: File not found"
+	FAIL=$(( $FAIL + 1 ))
 	return
     fi
     if [ $DEBUG -eq 0 ]; then
-	xmllint --noout --relaxng ../../docs/tdl.rng "$1" >& /dev/null
+	xmllint --noout --relaxng ../../docs/tdl.rng "$schema" >& /dev/null
     else
-	xmllint --relaxng ../../docs/tdl.rng "$1"
+	xmllint --relaxng ../../docs/tdl.rng "$schema"
     fi
-    if [ $? -ne 0 ]; then
-	echo "OK"
-	SUCCESS=$(( $SUCCESS + 1 ))
+
+    RET=$?
+
+    if [ $RET -eq 0 -a "$expectsuccess" = "true" ] || [ $RET -ne 0 -a "$expectsuccess" != "true" ]; then
+	success
     else
-	echo "FAIL"
-	FAIL=$(( $FAIL + 1 ))
+	failure
     fi
 }
 
-python_expect_success() {
+python_test() {
+    parse="$1"
+    expectsuccess="$2"
+
     echo -n "Testing Parsing $1..."
     if [ ! -r "$1" ]; then
-	echo "File not found"
+	echo "FAIL: File not found"
+	FAIL=$(( $FAIL + 1 ))
 	return
     fi
 
@@ -61,73 +62,43 @@ python_expect_success() {
 	python test_tdl.py "$1"
     fi
 
-    if [ $? -eq 0 ]; then
-	echo "OK"
-	SUCCESS=$(( $SUCCESS + 1 ))
-    else
-	echo "FAIL"
-	FAIL=$(( $FAIL + 1 ))
-    fi
-}
-python_expect_fail() {
-    echo -n "Testing Parsing $1..."
-    if [ ! -r "$1" ]; then
-	echo "File not found"
-	return
-    fi
+    RET=$?
 
-    if [ $DEBUG -eq 0 ]; then
-	python test_tdl.py "$1" >& /dev/null
+    if [ $RET -eq 0 -a "$expectsuccess" = "true" ] || [ $RET -ne 0 -a "$expectsuccess" != "true" ]; then
+	success
     else
-	python test_tdl.py "$1"
-    fi
-
-    if [ $? -ne 0 ]; then
-	echo "OK"
-	SUCCESS=$(( $SUCCESS + 1 ))
-    else
-	echo "FAIL"
-	FAIL=$(( $FAIL + 1 ))
+	failure
     fi
 }
 
-schema_expect_success test-01-simple-iso.tdl
-schema_expect_success test-02-simple-url.tdl
-schema_expect_fail test-03-empty-template.tdl
-schema_expect_fail test-04-no-os.tdl
-schema_expect_fail test-05-no-name.tdl
-schema_expect_success test-06-simple-iso-description.tdl
-schema_expect_success test-07-packages-no-package.tdl
-schema_expect_success test-08-repositories-no-repository.tdl
-schema_expect_fail test-09-os-invalid-arch.tdl
-schema_expect_fail test-10-os-invalid-install-type.tdl
-schema_expect_success test-11-description-packages-repositories.tdl
-schema_expect_fail test-12-os-no-name.tdl
-schema_expect_fail test-13-os-no-version.tdl
-schema_expect_fail test-14-os-no-arch.tdl
-schema_expect_fail test-15-os-no-install.tdl
-schema_expect_success test-16-signed-repository.tdl
-schema_expect_fail test-17-repo-invalid-signed.tdl
-schema_expect_success test-18-rootpw.tdl
+expect_success() {
+    schema_test "$1" "true"
+    python_test "$1" "true"
+}
 
-python_expect_success test-01-simple-iso.tdl
-python_expect_success test-02-simple-url.tdl
-python_expect_fail test-03-empty-template.tdl
-python_expect_fail test-04-no-os.tdl
-python_expect_fail test-05-no-name.tdl
-python_expect_success test-06-simple-iso-description.tdl
-python_expect_success test-07-packages-no-package.tdl
-python_expect_success test-08-repositories-no-repository.tdl
-python_expect_fail test-09-os-invalid-arch.tdl
-python_expect_fail test-10-os-invalid-install-type.tdl
-python_expect_success test-11-description-packages-repositories.tdl
-python_expect_fail test-12-os-no-name.tdl
-python_expect_fail test-13-os-no-version.tdl
-python_expect_fail test-14-os-no-arch.tdl
-python_expect_fail test-15-os-no-install.tdl
-python_expect_success test-16-signed-repository.tdl
-python_expect_fail test-17-repo-invalid-signed.tdl
-python_expect_success test-18-rootpw.tdl
+expect_fail() {
+    schema_test "$1" "false"
+    python_test "$1" "false"
+}
+
+expect_success test-01-simple-iso.tdl
+expect_success test-02-simple-url.tdl
+expect_fail test-03-empty-template.tdl
+expect_fail test-04-no-os.tdl
+expect_fail test-05-no-name.tdl
+expect_success test-06-simple-iso-description.tdl
+expect_success test-07-packages-no-package.tdl
+expect_success test-08-repositories-no-repository.tdl
+expect_fail test-09-os-invalid-arch.tdl
+expect_fail test-10-os-invalid-install-type.tdl
+expect_success test-11-description-packages-repositories.tdl
+expect_fail test-12-os-no-name.tdl
+expect_fail test-13-os-no-version.tdl
+expect_fail test-14-os-no-arch.tdl
+expect_fail test-15-os-no-install.tdl
+expect_success test-16-signed-repository.tdl
+expect_fail test-17-repo-invalid-signed.tdl
+expect_success test-18-rootpw.tdl
 
 echo "SUCCESS: $SUCCESS, FAIL: $FAIL"
 exit $FAIL
