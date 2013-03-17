5e992a28167f20a05f3a9e7de4a59dbfc0ba1ed9
Chris Lalancette <clalancette@gmail.com>
2013-03-17 11:38:13 -0400

    Add support for OpenSUSE 12.3
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- oz/OpenSUSE.py
+++ oz/OpenSUSE.py
@@ -560,11 +560,11 @@ def get_class(tdl, config, auto, output_disk=None):
     """
     if tdl.update in ["10.3"]:
         return OpenSUSEGuest(tdl, config, auto, output_disk, "rtl8139", "ide")
-    if tdl.update in ["11.0", "11.1", "11.2", "11.3", "11.4", "12.1", "12.2"]:
+    if tdl.update in ["11.0", "11.1", "11.2", "11.3", "11.4", "12.1", "12.2", "12.3"]:
         return OpenSUSEGuest(tdl, config, auto, output_disk, "virtio", "virtio")
 
 def get_supported_string():
     """
     Return supported versions as a string.
     """
-    return "OpenSUSE: 10.3, 11.0, 11.1, 11.2, 11.3, 11.4, 12.1, 12.2"
+    return "OpenSUSE: 10.3, 11.0, 11.1, 11.2, 11.3, 11.4, 12.1, 12.2, 12.3"
