75d82f4c2a2e54183f995b79881fc247517df340
helgi <helgith@gmail.com>
2013-12-02 12:51:07 -0800

    use os.path.expanduser on any path configuration in case user configuration contains ~/foo

--- oz/Guest.py
+++ oz/Guest.py
@@ -139,23 +139,27 @@ class Guest(object):
             self.macaddr = macaddress
 
         # configuration from 'paths' section
-        self.output_dir = oz.ozutil.config_get_key(config, 'paths',
-                                                   'output_dir',
-                                                   oz.ozutil.default_output_dir())
+        self.output_dir = os.path.expanduser(
+            oz.ozutil.config_get_key(config, 'paths',
+                                     'output_dir',
+                                     oz.ozutil.default_output_dir()))
 
         oz.ozutil.mkdir_p(self.output_dir)
 
-        self.data_dir = oz.ozutil.config_get_key(config, 'paths',
-                                                 'data_dir',
-                                                 oz.ozutil.default_data_dir())
+        self.data_dir = os.path.expanduser(
+            oz.ozutil.config_get_key(config, 'paths',
+                                     'data_dir',
+                                    oz.ozutil.default_data_dir()))
 
-        self.screenshot_dir = oz.ozutil.config_get_key(config, 'paths',
-                                                       'screenshot_dir',
-                                                       oz.ozutil.default_screenshot_dir(self.data_dir))
+        self.screenshot_dir = os.path.expanduser(
+            oz.ozutil.config_get_key(config, 'paths',
+                                     'screenshot_dir',
+                                     oz.ozutil.default_screenshot_dir(self.data_dir)))
 
-        self.sshprivkey = oz.ozutil.config_get_key(config, 'paths',
-                                                   'sshprivkey',
-                                                   oz.ozutil.default_sshprivkey())
+        self.sshprivkey = os.path.expanduser(
+            oz.ozutil.config_get_key(config, 'paths',
+                                     'sshprivkey',
+                                     oz.ozutil.default_sshprivkey()))
 
         # configuration from 'libvirt' section
         self.libvirt_uri = oz.ozutil.config_get_key(config, 'libvirt', 'uri',
