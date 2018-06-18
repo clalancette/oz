dc2b1fc0b5750cbb5a34addef56a14b6e9a30da9
Chris Lalancette <clalancette@gmail.com>
2018-06-18 18:45:05 -0400

    Revamp test_factory completely.
    This is to make it much more like py.test likes.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- tests/factory/test_factory.py
+++ tests/factory/test_factory.py
@@ -37,35 +37,6 @@ except ImportError:
     print('Unable to import py.test.  Is py.test installed?')
     sys.exit(1)
 
-# Define a list to collect all tests
-alltests = list()
-
-# Define an object to record test results
-class TestResult(object):
-    def __init__(self, *args, **kwargs):
-        if len(args) == 4:
-            (self.distro, self.version, self.arch, self.installtype) = args
-        for k,v in list(kwargs.items()):
-            setattr(self, k, v)
-
-    def __repr__(self):
-        '''String representation of object'''
-        return "test-{0}-{1}-{2}-{3}".format(*self.test_args())
-
-    @property
-    def name(self):
-        '''Convenience property for test name'''
-        return self.__repr__()
-
-    def test_args(self):
-        return (self.distro, self.version, self.arch, self.installtype)
-
-    def execute(self):
-        if self.expect_pass:
-            return (self.name, runtest, self.test_args())
-        else:
-            return (self.name, handle_exception, self.test_args())
-
 def default_route():
     route_file = "/proc/net/route"
     d = file(route_file)
@@ -89,9 +60,13 @@ def default_route():
 # doesn't really matter; it just has to have an IP address
 route = default_route()
 
-def runtest(args):
-    (distro, version, arch, installtype) = args
-    print("Testing %s-%s-%s-%s..." % (distro, version, arch, installtype), end=' ')
+def runtest(**kwargs):
+    distro = kwargs['distro']
+    version = kwargs['version']
+    arch = kwargs['arch']
+    installtype = kwargs['installtype']
+    expect_success = kwargs['expect_success']
+    print("Testing %s-%s-%s-%s" % (distro, version, arch, installtype))
 
     tdlxml = """
 <template>
@@ -101,206 +76,262 @@ def runtest(args):
     <version>%s</version>
     <arch>%s</arch>
     <install type='%s'>
-      <%s>http://example.org</%s>
+      <%s>file://example.org</%s>
     </install>
     <key>1234</key>
   </os>
 </template>
 """ % (distro, version, arch, installtype, installtype, installtype)
 
-    tdl = oz.TDL.TDL(tdlxml)
+    success = None
+    saved_exc = None
+    try:
+        tdl = oz.TDL.TDL(tdlxml)
 
-    print(route)
-    config = configparser.SafeConfigParser()
-    config.readfp(BytesIO("[libvirt]\nuri=qemu:///session\nbridge_name=%s" % route))
+        config = configparser.SafeConfigParser()
+        config.readfp(BytesIO("[libvirt]\nuri=qemu:///session\nbridge_name=%s" % route))
 
-    if os.getenv('DEBUG') is not None:
-        logging.basicConfig(level=logging.DEBUG, format="%(message)s")
-    else:
-        logging.basicConfig(level=logging.ERROR, format="%(message)s")
+        if os.getenv('DEBUG') is not None:
+            logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+        else:
+            logging.basicConfig(level=logging.ERROR, format="%(message)s")
 
-    oz.GuestFactory.guest_factory(tdl, config, None)
+            oz.GuestFactory.guest_factory(tdl, config, None)
+        if not expect_success:
+            assert(False)
+    except oz.OzException.OzException as e:
+        if expect_success:
+            raise
 
-def expect_success(*args):
-    '''Create a TestResult object using provided arguments.  Append result to
-    global 'alltests' list.'''
-    global alltests
-    alltests.append(TestResult(*args, expect_pass=True))
+def test_bad_distro():
+    runtest(distro='foo', version='1', arch='i386', installtype='url',
+            expect_success=False)
 
-def expect_fail(*args):
-    '''Create a TestResult object using provided arguments.  Append result to
-    global 'alltests' list.'''
-    global alltests
-    alltests.append(TestResult(*args, expect_pass=False))
+def test_bad_installtype():
+    runtest(distro='Fedora', version='14', arch='i386', installtype='duh',
+            expect_success=False)
 
-def handle_exception(args):
-    '''Helper method to capture OzException when executing 'runtest'.'''
-    with py.test.raises(oz.OzException.OzException):
-        runtest(args)
+def test_bad_arch():
+    runtest(distro='Fedora', version='14', arch='ia64', installtype='iso',
+            expect_success=False)
 
-def test_all():
-
-    # bad distro
-    expect_fail("foo", "1", "i386", "url")
-    # bad installtype
-    expect_fail("Fedora", "14", "i386", "dong")
-    # bad arch
-    expect_fail("Fedora", "14", "ia64", "iso")
-
-    # FedoraCore
+def test_fedora_core():
     for version in ["1", "2", "3", "4", "5", "6"]:
         for arch in ["i386", "x86_64"]:
             for installtype in ["url", "iso"]:
-                expect_success("FedoraCore", version, arch, installtype)
-    # bad FedoraCore version
-    expect_fail("FedoraCore", "24", "x86_64", "iso")
+                runtest(distro='FedoraCore', version=version, arch=arch,
+                        installtype=installtype, expect_success=True)
+    runtest(distro='FedoraCore', version='24', arch='x86_64', installtype='iso',
+            expect_success=False)
+    runtest(distro='FedoraCore', version='6', arch='ia64', installtype='iso',
+            expect_success=False)
+    runtest(distro='FedoraCore', version='6', arch='x86_64', installtype='foo',
+            expect_success=False)
 
-    # Fedora
+def test_fedora():
     for version in ["7", "8", "9", "10", "11", "12", "13", "14", "15", "16",
-                    "17", "18", "19", "20", "21", "22", "23"]:
+                    "17", "18", "19", "20", "21", "22", "23", "24", "25", "26",
+                    "27", "28"]:
         for arch in ["i386", "x86_64"]:
             for installtype in ["url", "iso"]:
-                expect_success("Fedora", version, arch, installtype)
+                runtest(distro='Fedora', version=version, arch=arch,
+                        installtype=installtype, expect_success=True)
+    runtest(distro='Fedora', version='1', arch='x86_64', installtype='iso',
+            expect_success=False)
+    runtest(distro='Fedora', version='1', arch='ia64', installtype='iso',
+            expect_success=False)
+    runtest(distro='Fedora', version='1', arch='x86_64', installtype='foo',
+            expect_success=False)
 
-    # RHL
+def test_rhl():
     for version in ["7.0", "7.1", "7.2", "7.3", "8", "9"]:
-        expect_success("RHL", version, "i386", "url")
-    # bad RHL version
-    expect_fail("RHL", "10", "i386", "url")
-    # bad RHL arch
-    expect_fail("RHL", "9", "x86_64", "url")
-    # bad RHL installtype
-    expect_fail("RHL", "9", "x86_64", "iso")
+        runtest(distro='RHL', version=version, arch='i386', installtype='url',
+                expect_success=True)
+    runtest(distro='RHL', version='10', arch='i386', installtype='url',
+            expect_success=False)
+    runtest(distro='RHL', version='9', arch='x86_64', installtype='url',
+            expect_success=False)
+    runtest(distro='RHL', version='9', arch='i386', installtype='iso',
+            expect_success=False)
 
-    # RHEL-2.1
+def test_rhel21():
     for version in ["GOLD", "U2", "U3", "U4", "U5", "U6"]:
-        expect_success("RHEL-2.1", version, "i386", "url")
-    # bad RHEL-2.1 version
-    expect_fail("RHEL-2.1", "U7", "i386", "url")
-    # bad RHEL-2.1 arch
-    expect_fail("RHEL-2.1", "U6", "x86_64", "url")
-    # bad RHEL-2.1 installtype
-    expect_fail("RHEL-2.1", "U6", "i386", "iso")
+        runtest(distro='RHEL-2.1', version=version, arch='i386',
+                installtype='url', expect_success=True)
+    runtest(distro='RHEL-2.1', version='U7', arch='i386', installtype='url',
+            expect_success=False)
+    runtest(distro='RHEL-2.1', version='U6', arch='x86_64', installtype='url',
+            expect_success=False)
+    runtest(distro='RHEL-2.1', version='U6', arch='i386', installtype='iso',
+            expect_success=False)
 
-    # RHEL-3/CentOS-3
+def test_rhel3():
     for distro in ["RHEL-3", "CentOS-3"]:
         for version in ["GOLD", "U1", "U2", "U3", "U4", "U5", "U6", "U7", "U8",
                         "U9"]:
             for arch in ["i386", "x86_64"]:
-                expect_success(distro, version, arch, "url")
-    # bad RHEL-3 version
-    expect_fail("RHEL-3", "U10", "x86_64", "url")
-    # invalid RHEL-3 installtype
-    expect_fail("RHEL-3", "U9", "x86_64", "iso")
+                runtest(distro=distro, version=version, arch=arch,
+                        installtype='url', expect_success=True)
+    runtest(distro='RHEL-3', version='U10', arch='x86_64', installtype='url',
+            expect_success=False)
+    runtest(distro='RHEL-3', version='U9', arch='ia64', installtype='iso',
+            expect_success=False)
+    runtest(distro='RHEL-3', version='U9', arch='x86_64', installtype='foo',
+            expect_success=False)
 
-    # RHEL-4/CentOS-4
+def test_rhel4():
     for distro in ["RHEL-4", "CentOS-4", "ScientificLinux-4"]:
         for version in ["GOLD", "U1", "U2", "U3", "U4", "U5", "U6", "U7", "U8",
                         "U9"]:
             for arch in ["i386", "x86_64"]:
                 for installtype in ["url", "iso"]:
-                    expect_success(distro, version, arch, installtype)
-    # bad RHEL-4 version
-    expect_fail("RHEL-4", "U10", "x86_64", "url")
+                    runtest(distro=distro, version=version, arch=arch,
+                            installtype=installtype, expect_success=True)
+    runtest(distro='RHEL-4', version='U10', arch='x86_64', installtype='url',
+            expect_success=False)
+    runtest(distro='RHEL-4', version='U9', arch='ia64', installtype='iso',
+            expect_success=False)
+    runtest(distro='RHEL-4', version='U9', arch='x86_64', installtype='foo',
+            expect_success=False)
 
-    # RHEL-5/CentOS-5
+def test_rhel5():
     for distro in ["RHEL-5", "CentOS-5", "ScientificLinux-5"]:
         for version in ["GOLD", "U1", "U2", "U3", "U4", "U5", "U6", "U7", "U8",
                         "U9", "U10", "U11"]:
             for arch in ["i386", "x86_64"]:
                 for installtype in ["url", "iso"]:
-                    expect_success(distro, version, arch, installtype)
-    # bad RHEL-5 version
-    expect_fail("RHEL-5", "U20", "x86_64", "url")
+                    runtest(distro=distro, version=version, arch=arch,
+                            installtype=installtype, expect_success=True)
+    runtest(distro='RHEL-5', version='U20', arch='x86_64', installtype='url',
+            expect_success=False)
+    runtest(distro='RHEL-5', version='U9', arch='ia64', installtype='iso',
+            expect_success=False)
+    runtest(distro='RHEL-5', version='U9', arch='x86_64', installtype='foo',
+            expect_success=False)
 
-    # RHEL-6
+def test_rhel6():
     for distro in ["RHEL-6", "CentOS-6", "ScientificLinux-6", "OEL-6"]:
         for version in ["0", "1", "2", "3", "4", "5", "6", "7", "8"]:
             for arch in ["i386", "x86_64"]:
                 for installtype in ["url", "iso"]:
-                    expect_success(distro, version, arch, installtype)
-    # bad RHEL-6 version
-    expect_fail("RHEL-6", "U10", "x86_64", "url")
+                    runtest(distro=distro, version=version, arch=arch,
+                            installtype=installtype, expect_success=True)
+    runtest(distro='RHEL-6', version='U9', arch='x86_64', installtype='url',
+            expect_success=False)
+    runtest(distro='RHEL-6', version='8', arch='ia64', installtype='iso',
+            expect_success=False)
+    runtest(distro='RHEL-6', version='8', arch='x86_64', installtype='foo',
+            expect_success=False)
 
-    # RHEL-7
+def test_rhel7():
     for distro in ["RHEL-7", "CentOS-7"]:
         for version in ["0", "1", "2"]:
             for arch in ["i386", "x86_64"]:
                 for installtype in ["url", "iso"]:
-                    expect_success(distro, version, arch, installtype)
-    # bad RHEL-7 version
-    expect_fail("RHEL-7", "U99", "x86_64", "url")
+                    runtest(distro=distro, version=version, arch=arch,
+                            installtype=installtype, expect_success=True)
+    runtest(distro='RHEL-7', version='U9', arch='x86_64', installtype='url',
+            expect_success=False)
+    runtest(distro='RHEL-7', version='2', arch='ia64', installtype='iso',
+            expect_success=False)
+    runtest(distro='RHEL-7', version='2', arch='x86_64', installtype='foo',
+            expect_success=False)
 
-    # Debian
-    for version in ["5", "6", "7", "8"]:
+def test_debian():
+    for version in ["5", "6", "7", "8", "9"]:
         for arch in ["i386", "x86_64"]:
-            expect_success("Debian", version, arch, "iso")
-    # bad Debian version
-    expect_fail("Debian", "12", "i386", "iso")
+            runtest(distro='Debian', version=version, arch=arch,
+                    installtype='iso', expect_success=True)
+    runtest(distro='Debian', version='U9', arch='x86_64', installtype='url',
+            expect_success=False)
+    runtest(distro='Debian', version='9', arch='ia64', installtype='iso',
+            expect_success=False)
+    runtest(distro='Debian', version='9', arch='x86_64', installtype='foo',
+            expect_success=False)
 
-    # Windows
-    expect_success("Windows", "2000", "i386", "iso")
-    for version in ["XP", "2003", "2008", "7", "8", "2012", "8.1"]:
+def test_windows():
+    runtest(distro='Windows', version='2000', arch='i386', installtype='iso',
+            expect_success=True)
+    for version in ["XP", "2003", "2008", "7", "8", "2012", "8.1", "2016", "10"]:
         for arch in ["i386", "x86_64"]:
-            expect_success("Windows", version, arch, "iso")
-    # bad Windows 2000 arch
-    expect_fail("Windows", "2000", "x86_64", "iso")
-    # bad Windows version
-    expect_fail("Windows", "1999", "x86_64", "iso")
-    # invalid Windows installtype
-    expect_fail("Windows", "2008", "x86_64", "url")
+            runtest(distro='Windows', version=version, arch=arch, installtype='iso',
+                    expect_success=True)
+    runtest(distro='Windows', version='U9', arch='x86_64', installtype='iso',
+            expect_success=False)
+    runtest(distro='Windows', version='2012', arch='ia64', installtype='iso',
+            expect_success=False)
+    runtest(distro='Windows', version='2012', arch='x86_64', installtype='foo',
+            expect_success=False)
+    runtest(distro='Windows', version='2000', arch='x86_64', installtype='iso',
+            expect_success=False)
 
-    # OpenSUSE
+def test_opensuse():
     for version in ["10.3", "11.0", "11.1", "11.2", "11.3", "11.4", "12.1",
                     "12.2", "12.3", "13.1", "13.2"]:
         for arch in ["i386", "x86_64"]:
-            expect_success("OpenSUSE", version, arch, "iso")
-    # bad OpenSUSE version
-    expect_fail("OpenSUSE", "16", "x86_64", "iso")
-    # invalid OpenSUSE installtype
-    expect_fail("OpenSUSE", "11.4", "x86_64", "url")
+            runtest(distro='OpenSUSE', version=version, arch=arch, installtype='iso',
+                    expect_success=True)
+    runtest(distro='OpenSUSE', version='U9', arch='x86_64', installtype='iso',
+            expect_success=False)
+    runtest(distro='OpenSUSE', version='13.2', arch='ia64', installtype='iso',
+            expect_success=False)
+    runtest(distro='OpenSUSE', version='13.2', arch='x86_64', installtype='foo',
+            expect_success=False)
 
-    # Ubuntu
+def test_ubuntu():
     for version in ["5.04", "5.10", "6.06", "6.06.1", "6.06.2", "6.10", "7.04",
                     "7.10", "8.04", "8.04.1", "8.04.2", "8.04.3", "8.04.4",
                     "8.10", "9.04", "9.10", "10.04", "10.04.1", "10.04.2",
                     "10.04.3", "10.10", "11.04", "11.10", "12.04", "12.04.1",
                     "12.04.2", "12.04.3", "12.10", "13.04", "13.10", "14.04",
-                    "14.10", "15.04", "15.10", "16.04"]:
+                    "14.10", "15.04", "15.10", "16.04", "16.10", "17.04",
+                    "17.10", "18.04"]:
         for arch in ["i386", "x86_64"]:
             for installtype in ["iso", "url"]:
-                expect_success("Ubuntu", version, arch, installtype)
-    # bad Ubuntu version
-    expect_fail("Ubuntu", "10.9", "i386", "iso")
+                runtest(distro='Ubuntu', version=version, arch=arch,
+                        installtype=installtype, expect_success=True)
+    runtest(distro='Ubuntu', version='U9', arch='x86_64', installtype='iso',
+            expect_success=False)
+    runtest(distro='Ubuntu', version='16.04', arch='ia64', installtype='iso',
+            expect_success=False)
+    runtest(distro='Ubuntu', version='16.04', arch='x86_64', installtype='foo',
+            expect_success=False)
 
-    # Mandrake
+def test_mandrake():
     for version in ["8.2", "9.1", "9.2", "10.0", "10.1"]:
-        expect_success("Mandrake", version, "i386", "iso")
-    # bad Mandrake version
-    expect_fail("Mandrake", "11", "i386", "iso")
-    # bad Mandrake arch
-    expect_fail("Mandrake", "8.2", "x86_64", "iso")
-    # bad Mandrake installtype
-    expect_fail("Mandrake", "8.2", "i386", "url")
+        runtest(distro='Mandrake', version=version, arch='i386',
+                installtype='iso', expect_success=True)
+    runtest(distro='Mandrake', version='U9', arch='x86_64', installtype='iso',
+            expect_success=False)
+    runtest(distro='Mandrake', version='10.1', arch='ia64', installtype='iso',
+            expect_success=False)
+    runtest(distro='Mandrake', version='10.1', arch='x86_64', installtype='foo',
+            expect_success=False)
+    runtest(distro='Mandrake', version='10.1', arch='x86_64', installtype='url',
+            expect_success=False)
 
-    # Mandriva
+def test_mandriva():
     for version in ["2005", "2006.0", "2007.0", "2008.0"]:
         for arch in ["i386", "x86_64"]:
-            expect_success("Mandriva", version, arch, "iso")
-    # bad Mandriva version
-    expect_fail("Mandriva", "80", "i386", "iso")
-    # bad Mandriva installtype
-    expect_fail("Mandriva", "2005", "i386", "url")
+            runtest(distro='Mandriva', version=version, arch=arch,
+                    installtype='iso', expect_success=True)
+    runtest(distro='Mandriva', version='U9', arch='x86_64', installtype='iso',
+            expect_success=False)
+    runtest(distro='Mandriva', version='2008.0', arch='ia64', installtype='iso',
+            expect_success=False)
+    runtest(distro='Mandriva', version='2008.0', arch='x86_64', installtype='foo',
+            expect_success=False)
+    runtest(distro='Mandriva', version='2008.0', arch='x86_64', installtype='url',
+            expect_success=False)
 
-    # Mageia
+def test_mageia():
     for version in ["2", "3", "4", "4.1", "5"]:
         for arch in ["i386", "x86_64"]:
-            expect_success("Mageia", version, arch, "iso")
-    # bad Mandriva version
-    expect_fail("Mageia", "80", "i386", "iso")
-    # bad Mandriva installtype
-    expect_fail("Mageia", "2005", "i386", "url")
-
-    # Now run all the tests
-    for tst in alltests:
-        yield tst.execute()
+            runtest(distro='Mageia', version=version, arch=arch,
+                    installtype='iso', expect_success=True)
+    runtest(distro='Mageia', version='U9', arch='x86_64', installtype='iso',
+            expect_success=False)
+    runtest(distro='Mageia', version='5', arch='ia64', installtype='iso',
+            expect_success=False)
+    runtest(distro='Mageia', version='5', arch='x86_64', installtype='foo',
+            expect_success=False)
